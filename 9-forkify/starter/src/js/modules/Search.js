dd783b4d6eea995f8d5cfedc04c91d254cb57b2c
Duong <30502963+phanthaiduong22@users.noreply.github.com>
2019-11-18 13:45:43 +0700

    last push

---
+++ 9-forkify/starter/src/js/modules/Search.js
@@ -0,0 +1,10 @@
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+}
+import axios from 'axios'
+
+async function getResults(query){
+    const proxy = ''
+}
